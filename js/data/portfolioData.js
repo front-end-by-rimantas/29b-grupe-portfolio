2db86a68bc86b4269161054876ac7ddba15541af
Rimantas <belauzas@yahoo.com>
2021-07-21 15:01:59 +0300

    Portfolio data

--- js/data/portfolioData.js
+++ js/data/portfolioData.js
@@ -1,3 +1,51 @@
-const portfolioData = [];
+const portfolioData = {
+    imgPath: './img/portfolio/',
+    maxItems: 6,
+    renderingStrategy: 'first',
+    list: [
+        {
+            img: '1.jpg',
+            alt: 'Portfolio item',
+            text: 'Web design',
+            link: '#',
+            tags: ['graphic'],
+        },
+        {
+            img: '2.jpg',
+            alt: 'Portfolio item',
+            text: 'Web design',
+            link: '#',
+            tags: ['design'],
+        },
+        {
+            img: '3.jpg',
+            alt: 'Portfolio item',
+            text: 'Web design',
+            link: '#',
+            tags: ['Graphic'],
+        },
+        {
+            img: '4.jpg',
+            alt: 'Portfolio item',
+            text: 'Web design',
+            link: '#',
+            tags: ['Brand'],
+        },
+        {
+            img: '5.jpg',
+            alt: 'Portfolio item',
+            text: 'Web design',
+            link: '#',
+            tags: ['Design', 'graphIc'],
+        },
+        {
+            img: '6.jpg',
+            alt: 'Portfolio item',
+            text: 'Web design',
+            link: '#',
+            tags: ['branD'],
+        },
+    ]
+}
 
 export { portfolioData }
